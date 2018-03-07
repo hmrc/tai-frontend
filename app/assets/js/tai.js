fc6c8468985e16b5a8673fc77f2df089abcecd3b
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2018-03-07 18:26:12 +0000

    Removing stale breadcrumb script - breadcrumb no longer present in service.

--- app/assets/js/tai.js
+++ app/assets/js/tai.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-
-	var $breadCrumb = $("#global-breadcrumb nav ol li:first-child a");
-	$breadCrumb.click(function(){
-		event.preventDefault();
-		$href = $(this).attr('href')
-		$path = $href.substring($href.lastIndexOf("/")+1);
-		ga('send', {
-			hitType: 'event',
-			eventCategory: 'check-income-tax',
-			eventAction: 'Breadcrumb Link - Account home',
-			eventLabel: "'"+$path+"'",
-			hitCallback: function() {
-				window.location.href = $href;
-			}
-		});
-	});
-
 	// Details/summary polyfill from frontend toolkit
 	GOVUK.details.init()
 });
