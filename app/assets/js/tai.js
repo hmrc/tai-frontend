2dc19a61458e8c4eb1abd671869cff30b23ad8c0
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2018-03-07 18:19:31 +0000

    Moving service specific script out of main template and into tai.js

--- app/assets/js/tai.js
+++ app/assets/js/tai.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	var $breadCrumb = $("#global-breadcrumb nav ol li:first-child a");
 	$breadCrumb.click(function(){
 		event.preventDefault();
@@ -24,3 +25,26 @@ $(document).ready(function() {
 window.onpageshow = function(event) {
     $('[type="submit"][disabled]').removeAttr('disabled');
 }
+
+/* Back link configuration */
+if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
+    window.history.replaceState(null, null, window.location.href);
+}
+var backLinkElem = document.getElementById("backLink");
+if (backLinkElem !=  null){
+    if (window.history && window.history.back && typeof window.history.back === 'function') {
+        var backScript = (document.referrer.indexOf(window.location.host) !== -1) ? "javascript:window.history.back();" : "javascript:void(0);"
+        backLinkElem.setAttribute("onclick",backScript);
+        backLinkElem.setAttribute("href","javascript:void(0);");
+    }
+}
+
+/* Temp - pending AF update to > 3.0.2 */
+window.GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
+  if (window.ga && typeof(window.ga) === 'function') {
+    ga('send', 'event', category, event, label);
+  } else {
+    _gaq.push(['_trackEvent', category, event, label, undefined, true]);
+  }
+};
+window.GOVUK.performance.stageprompt.setupForGoogleAnalytics();
