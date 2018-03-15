bf077e2679039378a227933253521b4073e04ef7
Adam Liptrot <adam.liptrot@opencastsoftware.com>
2018-03-15 14:08:15 +0000

    Fix tests

--- test/views/html/TaxFreeAmountNewViewSpec.scala
+++ test/views/html/TaxFreeAmountNewViewSpec.scala
@@ -105,7 +105,7 @@ class TaxFreeAmountNewViewSpec extends TaiViewSpec {
         doc must haveTableTdWithId("summaryTable1Row2ChangeLinkCell")
         doc must haveLinkWithUrlWithID("summaryTable1Row2ChangeLink", "/dummy/url2")
         doc must haveElementAtPathWithClass("a[id=summaryTable1Row2ChangeLink] > span", "visually-hidden")
-        doc must haveElementAtPathWithText("a[id=summaryTable1Row2ChangeLink] > span", messages("tai.change") + " context2")
+        doc must haveElementAtPathWithText("a[id=summaryTable1Row2ChangeLink] > span", messages("tai.updateOrRemove") + " context2")
       }
 
       "excludes a link cell from table rows, where instructed by the view model" in {
