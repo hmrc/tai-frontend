d763e3b14278b8cdd9b9a4e5e5e43cc6741a0a7d
Gaurav Gaur <gaurav.gaur@digital.hmrc.gov.uk>
2018-04-05 16:23:36 +0100

    TAI-3665 passed implicit messages to benefits and employment packages

--- app/uk/gov/hmrc/tai/forms/benefits/RemoveCompanyBenefitStopDateForm.scala
+++ app/uk/gov/hmrc/tai/forms/benefits/RemoveCompanyBenefitStopDateForm.scala
@@ -16,16 +16,14 @@
 
 package uk.gov.hmrc.tai.forms.benefits
 
-import play.api.Play.current
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.i18n.Messages
-import play.api.i18n.Messages.Implicits._
 import uk.gov.hmrc.tai.util.RemoveCompanyBenefitStopDateConstants
 
 object RemoveCompanyBenefitStopDateForm extends RemoveCompanyBenefitStopDateConstants{
 
-  val form: Form[Option[String]] = Form[Option[String]](
+  def form(implicit messages: Messages): Form[Option[String]] = Form[Option[String]](
     single(StopDateChoice -> optional(text).verifying(Messages("tai.error.chooseOneOption"), {_.isDefined}))
   )
 }
