f46e954d951fa3e0fe96d7c14da5ec3d27f63140
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2018-04-23 10:56:40 +0100

    Extract back link js from tai-new, and place in dedicated js file for loading from print pages. Mod to main template to include new file. Also removal of temporary GA js, now redundant with AF upgrade. Modification of page title in printwrapper - in line with GDS.

--- app/assets/js/tai-new.js
+++ app/assets/js/tai-new.js
@@ -41,27 +41,3 @@ $(document).ready(function() {
 window.onpageshow = function(event) {
     $('[type="submit"][disabled]').removeAttr('disabled');
 }
-
-/* Back link configuration */
-// store referrer value to cater for IE - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10474810/  */
-var docReferrer = document.referrer
-if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
-    window.history.replaceState(null, null, window.location.href);
-}
-var backLinkElem = document.getElementById("backLink");
-if (backLinkElem !=  null){
-    if (window.history && window.history.back && typeof window.history.back === 'function') {
-        var backScript = (docReferrer === "" || docReferrer.indexOf(window.location.host) !== -1) ? "javascript:window.history.back(); return false;" : "javascript:void(0);"
-        backLinkElem.setAttribute("onclick",backScript);
-        backLinkElem.setAttribute("href","javascript:void(0);");
-    }
-}
-
-/* Temp - pending AF update to > 3.0.2 */
-window.GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
-  if (window.ga && typeof(window.ga) === 'function') {
-    ga('send', 'event', category, event, label);
-  } else {
-    _gaq.push(['_trackEvent', category, event, label, undefined, true]);
-  }
-};
