3ef55631b12517fd209a99d87bcbf23f0067b9a2
Amaal Ali <amaal.ali@opencastsoftware.com>
2018-06-11 11:02:07 +0100

    ST-9: Add controller for Previous Year underpayment.

---
+++ test/controllers/PreviousYearUnderpaymentControllerSpec.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
+import play.api.test.Helpers._
+import play.api.inject.guice.GuiceApplicationBuilder
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class PreviousYearUnderpaymentControllerSpec extends PlaySpec with OneAppPerSuite {
+  override lazy val app = new GuiceApplicationBuilder().build()
+  def injector = app.injector
+        val controller =  new PreviousYearUnderpaymentController
+
+  "PreviousYearUnderpaymentController" should {
+    "respond with OK" when {
+      "underpaymentExplanation is called" in {
+
+        status(controller.underpaymentExplanation) mustEqual OK
+      }
+    }
+
+    "respond with UNAUTHORIZED" when {
+      "should kick user out if not authorized" in {
+        status(controller.underpaymentExplanation) mustEqual UNAUTHORIZED
+      }
+    }
+  }
+}
