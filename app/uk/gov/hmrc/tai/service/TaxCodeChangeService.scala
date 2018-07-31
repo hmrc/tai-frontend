70a08ddc4cbea63fb45727a1dd1a54c767d1d4e2
John Lewis <john.lewis@digital.hmrc.gov.uk>
2018-07-31 17:57:05 +0100

    ST-116:Service layer added

---
+++ app/uk/gov/hmrc/tai/service/TaxCodeChangeService.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tai.service
+
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.tai.connectors.TaxCodeChangeConnector
+import uk.gov.hmrc.tai.connectors.responses.TaiResponse
+
+import scala.concurrent.Future
+
+trait TaxCodeChangeService {
+
+  def taxCodeChangeConnector: TaxCodeChangeConnector
+
+  def taxCodeHistory(nino: Nino)(implicit hc: HeaderCarrier): Future[TaiResponse] = {
+    taxCodeChangeConnector.taxCodeHistory(nino)
+  }
+}
+
+object TaxCodeChangeService extends TaxCodeChangeService {
+  override val taxCodeChangeConnector: TaxCodeChangeConnector = TaxCodeChangeConnector
+}
