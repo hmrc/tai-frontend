9ebb122c692cd75b1bd69fa88a5505ab2c696092
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2018-08-06 14:51:52 +0100

    ST-183: Updating to remove space after currency amount

--- test/views/html/estimatedIncomeTax/zeroTaxEstimatedIncomeTaxSpec.scala
+++ test/views/html/estimatedIncomeTax/zeroTaxEstimatedIncomeTaxSpec.scala
@@ -69,11 +69,7 @@ class zeroTaxEstimatedIncomeTaxSpec extends TaiViewSpec {
       "the earnings for a NINO were lower than the tax free allowance" in {
         doc(view) must haveParagraphWithText(Html(messages("tai.estimatedIncomeLow.desc",
           messages("tai.estimatedIncome.taxFree.link"),
-          "£11,500 ")).body)
-
-        println("****** " + messages("tai.estimatedIncomeLow.desc",
-          messages("tai.estimatedIncome.taxFree.link"),
-          "£11,500 "))
+          "£11,500")).body)
 
         doc(view).select("#estimatedIncomeLowDesc").html() mustBe Html(Messages("tai.estimatedIncomeLow.desc",
           Link.toInternalPage(
@@ -81,7 +77,7 @@ class zeroTaxEstimatedIncomeTaxSpec extends TaiViewSpec {
             url = routes.TaxFreeAmountController.taxFreeAmount.url.toString,
             value = Some("tai.estimatedIncome.taxFree.link")
           ).toHtml,
-          "£11,500 ")).body
+          "£11,500")).body
 
         doc(view).select("#balanceEarningsDesc").html() mustBe Html(Messages("tai.estimatedIncomeEarning.desc",
           "£2,500")).body
