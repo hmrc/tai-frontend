ea6ea2c07f76849f5a4662e81f7b018cfceeb5cd
John Lewis <john.lewis@digital.hmrc.gov.uk>
2018-10-03 17:59:04 +0100

    ST-294:Testing correct sequence returned from check your answers viewmodel spec

--- test/uk/gov/hmrc/tai/viewModels/income/UpdateIncomeEstimateCheckYourAnswersViewModelSpec.scala
+++ test/uk/gov/hmrc/tai/viewModels/income/UpdateIncomeEstimateCheckYourAnswersViewModelSpec.scala
@@ -18,23 +18,68 @@ package uk.gov.hmrc.tai.viewModels.income
 
 import controllers.FakeTaiPlayApplication
 import org.scalatestplus.play.PlaySpec
-import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import uk.gov.hmrc.tai.viewModels.CheckYourAnswersConfirmationLine
 
 class UpdateIncomeEstimateCheckYourAnswersViewModelSpec extends PlaySpec with FakeTaiPlayApplication with I18nSupport{
 
   implicit val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
 
   "Update income estimate check your answers view model" must {
-    "return all monthly journey lines" when{
+    "return all journey lines" when{
       "taxable pay and bonus or overtime pay is provided" in{
         viewModel.journeyConfirmationLines.size mustBe 7
-
+        viewModel.journeyConfirmationLines mustEqual
+          Seq(paymentFrequencyAnswer,totalPayAnswer, hasDeductionAnswer, taxablePayAnswer, hasBonusOrOvertimeAnswer,
+            hasExtraBonusOrOvertimeAnswer, totalBonusOrOvertimeAnswer)
       }
     }
   }
 
+  lazy val paymentFrequencyAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.paymentFrequency"),
+    paymentFrequency,
+    controllers.routes.IncomeUpdateCalculatorController.payPeriodPage().url
+  )
+
+  lazy val totalPayAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.totalPay"),
+    totalPay,
+    controllers.routes.IncomeUpdateCalculatorController.payslipAmountPage().url
+  )
+
+  lazy val hasDeductionAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.hasDeduction"),
+    hasDeductions,
+    controllers.routes.IncomeUpdateCalculatorController.payslipDeductionsPage().url
+  )
+
+  lazy val taxablePayAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.taxablePay"),
+    taxablePay.get,
+    controllers.routes.IncomeUpdateCalculatorController.taxablePayslipAmountPage().url
+  )
+
+  lazy val hasBonusOrOvertimeAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.hasBonusOrOvertime"),
+    hasBonusOrOvertime,
+    controllers.routes.IncomeUpdateCalculatorController.bonusPaymentsPage().url
+  )
+
+  lazy val hasExtraBonusOrOvertimeAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.hasExtraBonusOrOvertime"),
+    hasExtraBonusOrOvertime.get,
+    controllers.routes.IncomeUpdateCalculatorController.bonusPaymentsPage().url
+  )
+
+  lazy val totalBonusOrOvertimeAnswer = CheckYourAnswersConfirmationLine(
+    Messages("tai.estimatedPay.update.checkYourAnswers.totalBonusOrOvertime"),
+    totalBonusOrOvertime.get,
+    controllers.routes.IncomeUpdateCalculatorController.bonusOvertimeAmountPage().url
+  )
+
   val paymentFrequency = "Monthly"
-  val totalMonthlyPay = "10000"
+  val totalPay = "10000"
   val hasDeductions ="Yes"
   val taxablePay = Some("1800")
   val hasBonusOrOvertime = "Yes"
@@ -43,7 +88,7 @@ class UpdateIncomeEstimateCheckYourAnswersViewModelSpec extends PlaySpec with Fa
 
   val viewModel = UpdateIncomeEstimateCheckYourAnswersViewModel(
     paymentFrequency,
-    totalMonthlyPay,
+    totalPay,
     hasDeductions,
     taxablePay,
     hasBonusOrOvertime,
