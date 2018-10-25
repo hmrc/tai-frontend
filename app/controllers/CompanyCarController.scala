736930b8acfa262c1feadec0c78d1ff348131f1a
Richard Bacon <richardjamesbaconhmrc@gmail.com>
2018-10-25 14:36:18 +0100

    DDCNLS-4923 removed redundant code from company car journey after link modification to paye company car pages.

--- app/controllers/CompanyCarController.scala
+++ app/controllers/CompanyCarController.scala
@@ -22,7 +22,7 @@ import controllers.audit.Auditable
 import controllers.auth.WithAuthorisedForTaiLite
 import uk.gov.hmrc.tai.forms.UpdateOrRemoveCarForm
 import uk.gov.hmrc.tai.forms.benefits.DateForm
-import uk.gov.hmrc.tai.viewModels.benefit.{CompanyCarCheckAnswersViewModel, CompanyCarChoiceViewModel}
+import uk.gov.hmrc.tai.viewModels.benefit.CompanyCarChoiceViewModel
 import play.api.Play.current
 import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
@@ -46,7 +46,7 @@ trait CompanyCarController extends TaiBaseController
   with WithAuthorisedForTaiLite
   with Auditable
   with JourneyCacheConstants
-  with FeatureTogglesConfig{
+  with FeatureTogglesConfig {
 
   def personService: PersonService
   def companyCarService: CompanyCarService
@@ -73,8 +73,8 @@ trait CompanyCarController extends TaiBaseController
               empId <- companyCarService.companyCarEmploymentId
               response <- companyCarService.beginJourney(Nino(user.getNino), empId)
             } yield response match {
-                case TaiSuccessResponseWithPayload(x: Map[String, String]) => Ok(views.html.benefits.updateCompanyCar(UpdateOrRemoveCarForm.createForm, CompanyCarChoiceViewModel(x)))
-                case TaiNoCompanyCarFoundResponse(_) => Redirect(ApplicationConfig.companyCarServiceUrl)
+              case TaiSuccessResponseWithPayload(x: Map[String, String]) => Ok(views.html.benefits.updateCompanyCar(UpdateOrRemoveCarForm.createForm, CompanyCarChoiceViewModel(x)))
+              case TaiNoCompanyCarFoundResponse(_) => Redirect(ApplicationConfig.companyCarServiceUrl)
             }
           }
   }
@@ -83,7 +83,6 @@ trait CompanyCarController extends TaiBaseController
     implicit user =>
       implicit person =>
         implicit request =>
-
           UpdateOrRemoveCarForm.createForm.bindFromRequest.fold(
             formWithErrors => {
               journeyCacheService.mandatoryValues(CompanyCar_CarModelKey, CompanyCar_CarProviderKey) flatMap { seq =>
@@ -101,111 +100,8 @@ trait CompanyCarController extends TaiBaseController
           )
   }
 
-  def getCompanyCarEndDate: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          ServiceCheckLite.personDetailsCheck {
-              journeyCacheService.currentValueAsDate(CompanyCar_DateGivenBackKey) map {
-                case Some(date) =>
-                  Ok(views.html.benefits.companyCarEndDate(DateForm(Messages("tai.companyCar.endDate.blank")).form.fill(date)))
-                case _ =>
-                  Ok(views.html.benefits.companyCarEndDate(DateForm(Messages("tai.companyCar.endDate.blank")).form))
-              }
-          }
-  }
-
-  def handleCompanyCarEndDate: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          journeyCacheService.currentCache flatMap { cachedData =>
-            val startDate = cachedData.get(CompanyCar_DateStartedKey)
-            DateForm.verifyDate(DateForm(Messages("tai.companyCar.endDate.blank")).form.bindFromRequest, startDate).fold(
-
-              formWithErrors => {
-                Future.successful(BadRequest(views.html.benefits.companyCarEndDate(formWithErrors)))
-              },
-
-              formData => {
-                journeyCacheService.cache(Map(CompanyCar_DateGivenBackKey -> formData.toString)) flatMap { res =>
-                  cachedData.get(CompanyCar_HasActiveFuelBenefitdKey) match {
-                    case Some(hasFuelBen) if isTrue(hasFuelBen) =>
-                      Future.successful(Redirect(controllers.routes.CompanyCarController.getFuelBenefitEndDate()))
-                    case _ =>
-                      Future.successful(Redirect(controllers.routes.CompanyCarController.checkYourAnswers()))
-                  }
-                }
-              }
-            )
-          }
-  }
-
-  def getFuelBenefitEndDate: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          ServiceCheckLite.personDetailsCheck {
-            journeyCacheService.currentValueAsDate(CompanyCar_DateFuelBenefitStoppedKey) map {
-              case Some(date) =>
-                Ok(views.html.benefits.fuelBenefitEndDate(DateForm(Messages("tai.companyCar.fuelBenefitEndDate.blank")).form.fill(date)))
-              case _ =>
-                Ok(views.html.benefits.fuelBenefitEndDate(DateForm(Messages("tai.companyCar.fuelBenefitEndDate.blank")).form))
-            }
-          }
-  }
-
-  def handleFuelBenefitEndDate: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          journeyCacheService.currentCache flatMap { cachedData =>
-            val checkDate = cachedData.get(CompanyCar_DateFuelBenefitStartedKey).orElse(cachedData.get(CompanyCar_DateStartedKey))
-            DateForm.verifyDate(DateForm(Messages("tai.companyCar.fuelBenefitEndDate.blank")).form.bindFromRequest, checkDate).fold(
-              formWithErrors => {
-                Future.successful(BadRequest(views.html.benefits.fuelBenefitEndDate((formWithErrors))))
-              },
-              formData => {
-                journeyCacheService.cache(CompanyCar_DateFuelBenefitStoppedKey, formData.toString) flatMap { _ =>
-                  Future.successful(Redirect(controllers.routes.CompanyCarController.checkYourAnswers()))
-                }
-              }
-            )
-          }
-  }
-
-  def checkYourAnswers: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          ServiceCheckLite.personDetailsCheck {
-            journeyCacheService.currentCache.map{cache=>
-              val viewModel = CompanyCarCheckAnswersViewModel(cache, TaxYear(TaxYearResolver.currentTaxYear))
-              Ok(views.html.benefits.companyCarCheckYourAnswers(viewModel))
-            }
-          }
-  }
-
-  def handleCheckYourAnswers: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          companyCarService.withdrawCompanyCarAndFuel(Nino(user.getNino), request.headers.get("Referer").getOrElse("NA")).map{_ =>
-            Redirect(controllers.routes.CompanyCarController.confirmation())
-          }
-  }
-
-  def confirmation: Action[AnyContent] = authorisedForTai(personService).async {
-    implicit user =>
-      implicit person =>
-        implicit request =>
-          ServiceCheckLite.personDetailsCheck {
-
-            Future.successful(Ok(views.html.benefits.companyCarConfirmation()))
-          }
-  }
-
 }
+
 // $COVERAGE-OFF$
 object CompanyCarController extends CompanyCarController with AuthenticationConnectors {
 
