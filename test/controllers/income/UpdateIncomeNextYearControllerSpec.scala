3e8f6cc009f31dc200e5563888864248976c3675
Brian Wakenshaw <brian.wakenshaw@digital.hmrc.gov.uk>
2018-11-08 15:16:35 +0000

    ST-367: Added initial test and implimentaion for start page controller

--- test/controllers/income/UpdateIncomeNextYearControllerSpec.scala
+++ test/controllers/income/UpdateIncomeNextYearControllerSpec.scala
@@ -16,19 +16,80 @@
 
 package controllers.income
 
-import controllers.FakeTaiPlayApplication
+import controllers.{AuthenticationConnectors, FakeTaiPlayApplication}
 import mocks.{MockPartialRetriever, MockTemplateRenderer}
+import org.mockito.Matchers
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
 import uk.gov.hmrc.play.partials.FormPartialRetriever
 import uk.gov.hmrc.renderer.TemplateRenderer
+import uk.gov.hmrc.tai.service.{PersonService, UpdateNextYearsIncomeService}
+import org.mockito.Mockito.when
+import uk.gov.hmrc.domain.Generator
+import org.mockito.Matchers.any
+import uk.gov.hmrc.tai.model.cache.UpdateNextYearsIncomeCacheModel
+import builders.{AuthBuilder, RequestBuilder, UserBuilder}
+import play.api.i18n.Messages.Implicits._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.audit.http.connector.AuditConnector
+import uk.gov.hmrc.play.frontend.auth.connectors.domain.Authority
+import uk.gov.hmrc.play.frontend.auth.connectors.{AuthConnector, DelegationConnector}
+import views.html.incomes.nextYear.updateIncomeCYPlus1Start
 
-class IncomeUpdateCalculatorControllerSpec extends PlaySpec
+import scala.concurrent.Future
+import scala.util.Random
+
+class UpdateIncomeNextYearControllerSpec extends PlaySpec
   with FakeTaiPlayApplication
   with MockitoSugar {
 
+  "start" must {
+    "return OK with the start view" when {
+      "income service returns full data" in {
+        val testController = createTestIncomeController
+        val employmentID = 1
+        val currentEstPay = 1234
+        val nino = generateNino
+        val model = UpdateNextYearsIncomeCacheModel("EmployerName", employmentID, currentEstPay)
+        when(testController.updateNextYearsIncomeService.setup(Matchers.eq(employmentID), Matchers.eq(nino))(any()))
+          .thenReturn(Future.successful(model))
+
+//        val expectedView = testController.startView(model)
+
+        val result = testController.start(employmentID)(RequestBuilder.buildFakeRequestWithOnlySession("GET"))
+        status(result) mustBe OK
+      }
+    }
+  }
+
+
+
+  private val generateNino = new Generator(new Random).nextNino
+
+  private def createTestIncomeController = new TestUpdateIncomeNextYearController
+
   private class TestUpdateIncomeNextYearController extends UpdateIncomeNextYearController {
+    override val personService = mock[PersonService]
     override implicit val templateRenderer: TemplateRenderer = MockTemplateRenderer
     override implicit val partialRetriever: FormPartialRetriever = MockPartialRetriever
+    override val updateNextYearsIncomeService = mock[UpdateNextYearsIncomeService]
+    override protected val delegationConnector: DelegationConnector = mock[DelegationConnector]
+    override protected val authConnector: AuthConnector = mock[AuthConnector]
+    override val auditConnector: AuditConnector = mock[AuditConnector]
+//    implicit val user = UserBuilder.apply()
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+
+    val ad: Future[Some[Authority]] = Future.successful(Some(AuthBuilder.createFakeAuthority(generateNino.toString())))
+    when(authConnector.currentAuthority(any(), any())).thenReturn(ad)
+    when(personService.personDetails(any())(any())).thenReturn(Future.successful(fakePerson(generateNino)))
+
+//    def startView(model: UpdateNextYearsIncomeCacheModel) = {
+//      implicit request: FakeRequest[_] => {
+//        updateIncomeCYPlus1Start(model)
+//      }
+//    }
+
   }
 }
