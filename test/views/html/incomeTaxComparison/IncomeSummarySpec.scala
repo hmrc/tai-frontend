159d4ea875e140e9bad3f12a718d6fc897b2303a
Sean Armstrong <5382335+SeanArmstrong@users.noreply.github.com>
2018-11-16 12:42:14 +0000

    ST-374: Remove update estimate CYPlus1 from pensions

--- test/views/html/incomeTaxComparison/IncomeSummarySpec.scala
+++ test/views/html/incomeTaxComparison/IncomeSummarySpec.scala
@@ -42,13 +42,12 @@ class IncomeSummarySpec extends TaiViewSpec {
       doc(view) must haveThWithText(employmentOneIncomeSourceDetail.name)
       doc(view) must haveTdWithText(employmentOneIncomeSourceDetail.amountCY)
       doc(view) must haveTdWithText(employmentOneIncomeSourceDetail.amountCYPlusOne)
-      doc(view) must haveTdWithText(s"Update estimated income for ${employmentOneIncomeSourceDetail.name}")
-
+      hasEstimatedIncomeLink(view, employmentTwoIncomeSourceDetail)
 
       doc(view) must haveThWithText(employmentTwoIncomeSourceDetail.name)
       doc(view) must haveTdWithText(employmentTwoIncomeSourceDetail.amountCY)
       doc(view) must haveTdWithText(employmentTwoIncomeSourceDetail.amountCYPlusOne)
-      doc(view) must haveTdWithText(s"Update estimated income for ${employmentTwoIncomeSourceDetail.name}")
+      hasEstimatedIncomeLink(view, employmentTwoIncomeSourceDetail)
     }
 
     "have income from pension header" in {
@@ -67,13 +66,10 @@ class IncomeSummarySpec extends TaiViewSpec {
       doc(viewPensionsOnly) must haveThWithText(pensionOneIncomeSourceDetail.name)
       doc(viewPensionsOnly) must haveTdWithText(pensionOneIncomeSourceDetail.amountCY)
       doc(viewPensionsOnly) must haveTdWithText(pensionOneIncomeSourceDetail.amountCYPlusOne)
-      doc(viewPensionsOnly) must haveTdWithText(s"Update estimated income for ${pensionOneIncomeSourceDetail.name}")
-
 
       doc(viewPensionsOnly) must haveThWithText(pensionTwoIncomeSourceDetail.name)
       doc(viewPensionsOnly) must haveTdWithText(pensionTwoIncomeSourceDetail.amountCY)
       doc(viewPensionsOnly) must haveTdWithText(pensionTwoIncomeSourceDetail.amountCYPlusOne)
-      doc(viewPensionsOnly) must haveTdWithText(s"Update estimated income for ${pensionTwoIncomeSourceDetail.name}")
 
     }
 
@@ -92,22 +88,20 @@ class IncomeSummarySpec extends TaiViewSpec {
       doc(viewCombined) must haveThWithText(employmentOneIncomeSourceDetail.name)
       doc(viewCombined) must haveTdWithText(employmentOneIncomeSourceDetail.amountCY)
       doc(viewCombined) must haveTdWithText(employmentOneIncomeSourceDetail.amountCYPlusOne)
-      doc(viewCombined) must haveTdWithText(s"Update estimated income for ${employmentOneIncomeSourceDetail.name}")
+      hasEstimatedIncomeLink(view, employmentOneIncomeSourceDetail)
 
       doc(viewCombined) must haveThWithText(employmentTwoIncomeSourceDetail.name)
       doc(viewCombined) must haveTdWithText(employmentTwoIncomeSourceDetail.amountCY)
       doc(viewCombined) must haveTdWithText(employmentTwoIncomeSourceDetail.amountCYPlusOne)
-      doc(viewCombined) must haveTdWithText(s"Update estimated income for ${employmentTwoIncomeSourceDetail.name}")
+      hasEstimatedIncomeLink(view, employmentTwoIncomeSourceDetail)
 
       doc(viewCombined) must haveThWithText(pensionOneIncomeSourceDetail.name)
       doc(viewCombined) must haveTdWithText(pensionOneIncomeSourceDetail.amountCY)
       doc(viewCombined) must haveTdWithText(pensionOneIncomeSourceDetail.amountCYPlusOne)
-      doc(viewCombined) must haveTdWithText(s"Update estimated income for ${pensionOneIncomeSourceDetail.name}")
 
       doc(viewCombined) must haveThWithText(pensionTwoIncomeSourceDetail.name)
       doc(viewCombined) must haveTdWithText(pensionTwoIncomeSourceDetail.amountCY)
       doc(viewCombined) must haveTdWithText(pensionTwoIncomeSourceDetail.amountCYPlusOne)
-      doc(viewCombined) must haveTdWithText(s"Update estimated income for ${pensionTwoIncomeSourceDetail.name}")
     }
 
     "display no content when no CY or CY+1 details are available" in{
@@ -118,7 +112,14 @@ class IncomeSummarySpec extends TaiViewSpec {
 
       doc(viewNoDetails) mustNot haveElementWithId("incomeSummaryComparisonTable")
     }
+  }
 
+  private def hasEstimatedIncomeLink(view: Html, income: IncomeSourceComparisonDetail) = {
+    doc(viewCombined) must haveLinkElement(
+      s"estimated-income-link-${income.empId}",
+      s"/check-income-tax/update-income/next-year/income/${income.empId}/start",
+      s"Update estimated income for ${income.name}"
+    )
   }
 
   private lazy val employmentOneIncomeSourceDetail = IncomeSourceComparisonDetail(1, "Company1","£15,000","£15,500")
