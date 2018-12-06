e3140f52caf954dad76037041284ca2406727315
Sean Armstrong <5382335+SeanArmstrong@users.noreply.github.com>
2018-12-06 13:53:18 +0000

    ST-366-2 wip

--- app/uk/gov/hmrc/tai/util/YourTaxFreeAmount.scala
+++ app/uk/gov/hmrc/tai/util/YourTaxFreeAmount.scala
@@ -1,5 +1,32 @@
 package uk.gov.hmrc.tai.util
 
-trait YourTaxFreeAmount {
+import org.joda.time.LocalDate
+import play.api.i18n.Messages
+import uk.gov.hmrc.play.views.helpers.MoneyPounds
+import uk.gov.hmrc.tai.model.domain.benefits.CompanyCarBenefit
+import uk.gov.hmrc.tai.model.domain.calculation.CodingComponent
+import uk.gov.hmrc.tai.viewModels.{TaxFreeAmountSummaryViewModel, TaxFreeAmountViewModel}
+import uk.gov.hmrc.tai.viewModels.taxCodeChange.YourTaxFreeAmountViewModel
+import uk.gov.hmrc.time.TaxYearResolver
 
+
+trait YourTaxFreeAmount extends ViewModelHelper with TaxAccountCalculator {
+  def buildTaxFreeAmount(recentTaxCodeChangeDate: LocalDate,
+                         codingComponents: Seq[CodingComponent],
+                         employmentNames: Map[Int, String],
+                         companyCarBenefits: Seq[CompanyCarBenefit])
+                        (implicit messages: Messages): YourTaxFreeAmountViewModel = {
+    val taxCodeDateRange = TaxYearRangeUtil.dynamicDateRange(recentTaxCodeChangeDate, TaxYearResolver.endOfCurrentTaxYear)
+
+    val taxFreeAmountTotal = taxFreeAmount(codingComponents)
+    val annualTaxFreeAmount = withPoundPrefixAndSign(MoneyPounds (taxFreeAmountTotal, 0) )
+    val taxFreeAmountSummary = TaxFreeAmountSummaryViewModel(codingComponents, employmentNames, companyCarBenefits, taxFreeAmountTotal)
+
+    YourTaxFreeAmountViewModel(
+      taxFreeAmountTotal,
+      taxCodeDateRange,
+      annualTaxFreeAmount,
+      taxFreeAmountSummary
+    )
+  }
 }
--- test/uk/gov/hmrc/tai/util/YourTaxFreeAmountSpec.scala
+++ test/uk/gov/hmrc/tai/util/YourTaxFreeAmountSpec.scala
@@ -1,7 +1,37 @@
 package uk.gov.hmrc.tai.util
 
 import org.scalatestplus.play.PlaySpec
+import uk.gov.hmrc.tai.model.domain.calculation.CodingComponent
+import uk.gov.hmrc.tai.viewModels.TaxFreeAmountViewModel
 
 class YourTaxFreeAmountSpec extends PlaySpec {
-  ""
+  "builds tax free amount view model" in {
+    val taxFreeAmount = new TestTaxFreeAmount
+
+
+  }
+
+  class TaxAccountCalcuatorMock { this: TaxAccountCalculator =>
+    override def taxFreeAmount(codingComponents: Seq[CodingComponent])
+  }
+
+  class TestTaxFreeAmount extends YourTaxFreeAmount {
+    override def dynamicDateRangeHtmlNonBreak(): String {
+        "abc"
+    }
+
+//    val taxCodeDateRange = TaxYearRangeUtil.dynamicDateRange(recentTaxCodeChangeDate, TaxYearResolver.endOfCurrentTaxYear)
+//
+//    val taxFreeAmountTotal = taxFreeAmount(codingComponents)
+//    val annualTaxFreeAmount = withPoundPrefixAndSign(MoneyPounds (taxFreeAmountTotal, 0) )
+//    val taxFreeAmountSummary = TaxFreeAmountSummaryViewModel(codingComponents, employmentNames, companyCarBenefits, taxFreeAmountTotal)
+//
+//    YourTaxFreeAmountViewModel(
+//      taxFreeAmountTotal,
+//      taxCodeDateRange,
+//      annualTaxFreeAmount,
+//      taxFreeAmountSummary
+    }
+  }
+
 }
