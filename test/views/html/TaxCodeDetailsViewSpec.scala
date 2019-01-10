1623782a11d3c9736576911ece6c8cf5e13190ea
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2019-01-10 09:36:36 +0000

    ST-475: Company car controller now uses new auth

--- test/views/html/TaxCodeDetailsViewSpec.scala
+++ test/views/html/TaxCodeDetailsViewSpec.scala
@@ -25,8 +25,8 @@ import scala.collection.immutable.ListMap
 import scala.util.Random
 
 class TaxCodeDetailsViewSpec extends TaiViewSpec {
-  def generateNino = new Generator(new Random).nextNino.toString()
-  implicit val authActionedTaiUser: AuthActionedTaiUser = AuthActionedTaiUser("Firstname Surname", generateNino, "utr")
+  override def generateNino = new Generator(new Random).nextNino.toString()
+  override implicit val authActionedTaiUser: AuthActionedTaiUser = AuthActionedTaiUser("Firstname Surname", generateNino, "utr")
 
   "Tax code view page" must {
     behave like pageWithTitle("main heading")
