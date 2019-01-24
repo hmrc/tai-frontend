33b68d3b2d726339ea25f9336a635b6c3388dd74
Steven Truong <45595406+oc-ste@users.noreply.github.com>
2019-01-24 15:59:42 +0000

    Authed up IncomeSourceSummaryController

--- test/controllers/IncomeSourceSummaryControllerSpec.scala
+++ test/controllers/IncomeSourceSummaryControllerSpec.scala
@@ -16,7 +16,8 @@
 
 package controllers
 
-import builders.{AuthBuilder, RequestBuilder}
+import builders.RequestBuilder
+import controllers.actions.FakeValidatePerson
 import mocks.MockTemplateRenderer
 import org.joda.time.LocalDate
 import org.jsoup.Jsoup
@@ -28,7 +29,6 @@ import play.api.i18n.{I18nSupport, Messages, MessagesApi}
 import play.api.test.Helpers.{status, _}
 import uk.gov.hmrc.domain.Generator
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
-import uk.gov.hmrc.play.frontend.auth.connectors.{AuthConnector, DelegationConnector}
 import uk.gov.hmrc.play.partials.FormPartialRetriever
 import uk.gov.hmrc.tai.connectors.responses.{TaiSuccessResponseWithPayload, TaiTaxAccountFailureResponse}
 import uk.gov.hmrc.tai.model.TaxYear
@@ -36,7 +36,7 @@ import uk.gov.hmrc.tai.model.domain._
 import uk.gov.hmrc.tai.model.domain.benefits.{Benefits, CompanyCarBenefit, GenericBenefit}
 import uk.gov.hmrc.tai.model.domain.income.{Live, OtherBasisOfOperation, TaxCodeIncome, Week1Month1BasisOfOperation}
 import uk.gov.hmrc.tai.service.benefits.BenefitsService
-import uk.gov.hmrc.tai.service.{EmploymentService, PersonService, TaxAccountService}
+import uk.gov.hmrc.tai.service.{EmploymentService, TaxAccountService}
 
 import scala.concurrent.Future
 import scala.util.Random
@@ -128,26 +128,18 @@ class IncomeSourceSummaryControllerSpec extends PlaySpec
 
   def createSUT = new SUT
 
-  val personService: PersonService = mock[PersonService]
   val benefitsService = mock[BenefitsService]
   val employmentService = mock[EmploymentService]
   val taxAccountService = mock[TaxAccountService]
 
   class SUT extends IncomeSourceSummaryController(
-    personService,
     mock[AuditConnector],
-    mock[DelegationConnector],
-    mock[AuthConnector],
     taxAccountService,
     employmentService,
     benefitsService,
+    FakeAuthAction,
+    FakeValidatePerson,
     mock[FormPartialRetriever],
     MockTemplateRenderer
-  ) {
-
-    when(personService.personDetails(any())(any())).thenReturn(Future.successful(fakePerson(nino)))
-    when(authConnector.currentAuthority(any(), any())).thenReturn(AuthBuilder.createFakeAuthData)
-
-  }
-
+  )
 }
