e5e6b9f5f5e47f52bf7cbac43762c09a850908c3
Steven Truong <45595406+oc-ste@users.noreply.github.com>
2019-02-05 10:10:34 +0000

    revert TaxAccountSummaryController to use old auth so users can log in via verify

--- test/controllers/TaxAccountSummaryControllerSpec.scala
+++ test/controllers/TaxAccountSummaryControllerSpec.scala
@@ -16,14 +16,13 @@
 
 package controllers
 
-import builders.RequestBuilder
-import controllers.actions.FakeValidatePerson
+import builders.{AuthBuilder, RequestBuilder}
 import mocks.MockTemplateRenderer
 import org.joda.time.LocalDate
 import org.jsoup.Jsoup
+import org.mockito.{Matchers, Mockito}
 import org.mockito.Matchers.any
 import org.mockito.Mockito.{times, verify, when}
-import org.mockito.{Matchers, Mockito}
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.PlaySpec
@@ -33,6 +32,7 @@ import uk.gov.hmrc.domain.Generator
 import uk.gov.hmrc.http.BadRequestException
 import uk.gov.hmrc.play.audit.http.connector.AuditConnector
 import uk.gov.hmrc.play.audit.http.connector.AuditResult.Success
+import uk.gov.hmrc.play.frontend.auth.connectors.{AuthConnector, DelegationConnector}
 import uk.gov.hmrc.play.partials.FormPartialRetriever
 import uk.gov.hmrc.tai.connectors.responses.{TaiSuccessResponseWithPayload, TaiTaxAccountFailureResponse}
 import uk.gov.hmrc.tai.model.domain._
@@ -153,6 +153,7 @@ class TaxAccountSummaryControllerSpec extends PlaySpec
       "a downstream error has occurred in one of the TaiResponse responding service methods due to no found primary employment information" in {
         val sut = createSUT
         when(trackingService.isAnyIFormInProgress(any())(any())).thenReturn(Future.successful(true))
+        when(sut.authConnector.currentAuthority(any(), any())).thenReturn(AuthBuilder.createFakeAuthData(nino))
         when(personService.personDetails(any())(any())).thenReturn(Future.successful(fakePerson(nino)))
 
         when(taxAccountService.taxAccountSummary(any(), any())(any())).thenReturn(Future(TaiTaxAccountFailureResponse(TaiConstants.NpsTaxAccountDataAbsentMsg.toLowerCase)))
@@ -165,6 +166,7 @@ class TaxAccountSummaryControllerSpec extends PlaySpec
       "a downstream error has occurred in one of the TaiResponse responding service methods due to no employments recorded for current tax year" in {
         val sut = createSUT
         when(trackingService.isAnyIFormInProgress(any())(any())).thenReturn(Future.successful(true))
+        when(sut.authConnector.currentAuthority(any(), any())).thenReturn(AuthBuilder.createFakeAuthData(nino))
         when(personService.personDetails(any())(any())).thenReturn(Future.successful(fakePerson(nino)))
 
         when(taxAccountService.taxAccountSummary(any(), any())(any())).thenReturn(Future(TaiTaxAccountFailureResponse(TaiConstants.NpsNoEmploymentForCurrentTaxYear.toLowerCase)))
@@ -175,9 +177,11 @@ class TaxAccountSummaryControllerSpec extends PlaySpec
 
       }
     }
+
   }
 
-  val nino = FakeAuthAction.nino
+
+  val nino = new Generator(new Random).nextNino
 
   val employment = Employment("employment1", None, new LocalDate(), None, Nil, "", "", 1, None, false, false)
 
@@ -205,11 +209,16 @@ class TaxAccountSummaryControllerSpec extends PlaySpec
     employmentService,
     taxAccountService,
     auditService,
+    personService,
     mock[AuditConnector],
-    FakeAuthAction,
-    FakeValidatePerson,
+    mock[DelegationConnector],
+    mock[AuthConnector],
     mock[FormPartialRetriever],
     MockTemplateRenderer
-  )
+  ) {
+
+    when(authConnector.currentAuthority(any(), any())).thenReturn(AuthBuilder.createFakeAuthData(nino))
+    when(personService.personDetails(any())(any())).thenReturn(Future.successful(fakePerson(nino)))
+  }
 
 }
