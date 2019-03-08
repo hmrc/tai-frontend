939e93e1986926f03a26b414df108bbfa24c488a
Steven Truong <45595406+oc-ste@users.noreply.github.com>
2019-03-08 13:43:07 +0000

    Reinstated a test

--- test/views/html/incomes/PaySlipAmountSpec.scala
+++ test/views/html/incomes/PaySlipAmountSpec.scala
@@ -14,39 +14,44 @@
  * limitations under the License.
  */
 
-//package views.html.incomes
-//
-//import org.mockito.Matchers._
-//import org.mockito.Mockito._
-//import org.scalatest.mockito.MockitoSugar
-//import play.api.data.{Field, Form}
-//import play.api.mvc.Call
-//import play.twirl.api.Html
-//import uk.gov.hmrc.tai.forms.PayslipForm
-//import uk.gov.hmrc.tai.util.viewHelpers.TaiViewSpec
-//
-//class PaySlipAmountSpec extends TaiViewSpec with MockitoSugar {
-//
-//  val id = 1
-//  val employerName = "Employer"
-//
-//  "Pay slip amount view" should {
-//    behave like pageWithBackLink
-//    behave like pageWithCancelLink(Call("GET", controllers.routes.IncomeSourceSummaryController.onPageLoad(id).url))
-//    behave like pageWithCombinedHeader(
-//      messages("tai.payslip.preHeading", employerName),
-//      messages("tai.payslip.heading"))
-//  }
-//
-//  val payslipForm = mock[Form[PayslipForm]]
-//
-//  val field = mock[Field]
-//  when(field.value).thenReturn(Some("fakeFieldValue"))
-//  when(field.name).thenReturn("fakeFieldValue")
-//  when(payslipForm(any())).thenReturn(field)
-//  when(payslipForm.errors).thenReturn(Nil)
-//  when(payslipForm.errors(anyString())).thenReturn(Nil)
-//  when(payslipForm.hasErrors).thenReturn(false)
-//
-//  override def view: Html = views.html.incomes.payslipAmount(payslipForm, "", id, employerName)
-//}
+package views.html.incomes
+
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.mockito.MockitoSugar
+import play.api.data.{Field, Form}
+import play.api.mvc.Call
+import play.twirl.api.Html
+import uk.gov.hmrc.tai.forms.PayslipForm
+import uk.gov.hmrc.tai.util.constants.EditIncomePayPeriodConstants
+import uk.gov.hmrc.tai.util.viewHelpers.TaiViewSpec
+import uk.gov.hmrc.tai.viewModels.income.estimatedPay.update.PaySlipAmountViewModel
+
+class PaySlipAmountSpec extends TaiViewSpec with MockitoSugar with EditIncomePayPeriodConstants {
+
+  val id = 1
+  val employerName = "Employer"
+
+  "Pay slip amount view" should {
+    behave like pageWithBackLink
+    behave like pageWithCancelLink(Call("GET", controllers.routes.IncomeSourceSummaryController.onPageLoad(id).url))
+    behave like pageWithCombinedHeader(
+      messages("tai.payslip.preHeading", employerName),
+      messages("tai.payslip.title.month"))
+  }
+
+  val payslipForm = mock[Form[PayslipForm]]
+
+  val field = mock[Field]
+  when(field.value).thenReturn(Some("fakeFieldValue"))
+  when(field.name).thenReturn("fakeFieldValue")
+  when(payslipForm(any())).thenReturn(field)
+  when(payslipForm.errors).thenReturn(Nil)
+  when(payslipForm.errors(anyString())).thenReturn(Nil)
+  when(payslipForm.hasErrors).thenReturn(false)
+
+
+  val payslipViewModel = PaySlipAmountViewModel(payslipForm, Some(MONTHLY), None, id, employerName)
+
+  override def view: Html = views.html.incomes.payslipAmount(payslipViewModel)
+}
