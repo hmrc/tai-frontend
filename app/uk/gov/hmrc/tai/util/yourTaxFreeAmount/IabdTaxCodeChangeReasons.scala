17096491312f5e8b2cb9242b991158bdfb0a4a12
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-03-25 14:41:57 +0000

    ST-589: Changed IabdTaxCodeChangeReasons to seperate Allowances and Deductions

--- app/uk/gov/hmrc/tai/util/yourTaxFreeAmount/IabdTaxCodeChangeReasons.scala
+++ app/uk/gov/hmrc/tai/util/yourTaxFreeAmount/IabdTaxCodeChangeReasons.scala
@@ -26,12 +26,23 @@ class IabdTaxCodeChangeReasons(totalTax: TotalTax)  {
 
   def reasons(iabdPairs: AllowancesAndDeductionPairs)(implicit messages: Messages): Seq[String] = {
 
-    val combinedBenefits = iabdPairs.allowances ++ iabdPairs.deductions
+    allowanceReasons(iabdPairs) ++ deductionReasons(iabdPairs)
+    
+  }
 
-    val whatsChangedPairs = combinedBenefits.filter(pair => pair.previous.isDefined && pair.current.isDefined)
-    val whatsNewPairs = combinedBenefits.filter(pair => pair.previous.isEmpty && pair.current.isDefined)
-    val whatsNewUnderpaymentPairs = whatsNewPairs.filter(pair => pair.componentType == UnderPaymentFromPreviousYear || pair.componentType == EstimatedTaxYouOweThisYear)
-    val whatsNewOtherPairs = whatsNewPairs.filterNot(pair => pair.componentType == UnderPaymentFromPreviousYear || pair.componentType == EstimatedTaxYouOweThisYear)
+  private def allowanceReasons(iabdPairs: AllowancesAndDeductionPairs)(implicit messages: Messages): Seq[String] = {
+    val whatsChangedPairs = iabdPairs.allowances.filter(pair => pair.previous.isDefined && pair.current.isDefined)
+    val whatsNewPairs = iabdPairs.allowances.filter(pair => pair.previous.isEmpty && pair.current.isDefined)
+
+    whatsNewPairs.map(translateNewBenefits(_)) ++
+      whatsChangedPairs.flatMap(translateChangedBenefits(_))
+  }
+
+  private def deductionReasons(iabdPairs: AllowancesAndDeductionPairs)(implicit messages: Messages): Seq[String] = {
+    val whatsChangedPairs = iabdPairs.deductions.filter(pair => pair.previous.isDefined && pair.current.isDefined)
+    val whatsNewPairs = iabdPairs.deductions.filter(pair => pair.previous.isEmpty && pair.current.isDefined)
+    val whatsNewUnderpaymentPairs = whatsNewPairs.filter(pair => pair.componentType == UnderPaymentFromPreviousYear || pair.componentType == EstimatedTaxYouOweThisYear || pair.componentType == EarlyYearsAdjustment)
+    val whatsNewOtherPairs = whatsNewPairs.filterNot(pair => pair.componentType == UnderPaymentFromPreviousYear || pair.componentType == EstimatedTaxYouOweThisYear || pair.componentType == EarlyYearsAdjustment)
 
     whatsNewOtherPairs.map(translateNewBenefits(_)) ++
       whatsChangedPairs.flatMap(translateChangedBenefits(_)) ++
