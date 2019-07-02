57642c1c5ce20f44731171aa850f4bb2d4c03048
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2019-07-02 15:01:17 +0100

    DDCNLS-5913 welsh added an MessageSpec failing test corrected

--- app/controllers/ErrorPagesHandler.scala
+++ app/controllers/ErrorPagesHandler.scala
@@ -152,7 +152,6 @@ trait ErrorPagesHandler {
 
         case e: Upstream5xxResponse => {
           Logger.warn(s"<Upstream5xxResponse> - $methodName nino $nino")
-          println("\n\n\n\n\n upstream")
           Future.successful(InternalServerError(error5xx(
             Messages("tai.technical.error.npsdown.message"))))
         }
