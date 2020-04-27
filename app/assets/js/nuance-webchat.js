9abb8b0838c2849300b5eb7214de1738be9ea2cc
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2020-04-27 11:54:00 +0100

    [DEP-226] - ID36 Revised webchat tabbed position TAAS (TES,TCS, DEP-FE)

---
+++ app/assets/js/nuance-webchat.js
@@ -0,0 +1,26 @@
+// dynamically re-position nuance divs before footer for accessibility
+
+$(window).on("load", function() {
+
+    var waitForEl = function(selector, callback, count) {
+        if (jQuery(selector).length) {
+        callback();
+        } else {
+        setTimeout(function() {
+          if(!count) {
+            count=0;
+          }
+          count++;
+          if(count<4) {
+            waitForEl(selector,callback,count);
+          } else {return;}
+        }, 500);
+      }
+    }
+
+    waitForEl("#inqChatStage", function() {
+        $("#Nuance-chat-anchored").appendTo("#wrapper #content #skipto article");
+        $("#inqChatStage").appendTo("#wrapper #content #skipto article");
+    });
+
+});
