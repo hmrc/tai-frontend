5c495b508f08c8823862a141483a882012193f2c
Steven Truong <45595406+struong@users.noreply.github.com>
2020-10-16 11:24:14 +0100

    DDCNL-2250: Moved genuine external URLs to the external URLs in application.conf so we can override them easily in non-local

--- app/uk/gov/hmrc/tai/config/ApplicationConfig.scala
+++ app/uk/gov/hmrc/tai/config/ApplicationConfig.scala
@@ -18,46 +18,38 @@ package uk.gov.hmrc.tai.config
 
 import javax.inject.Inject
 import play.api.{ConfigLoader, Configuration}
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.play.bootstrap.binders.SafeRedirectUrl
+import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
 import uk.gov.hmrc.tai.model.TaxYear
-import views.html.helper
 
 class ApplicationConfig @Inject()(
   val runModeConfiguration: Configuration,
+  runMode: RunMode,
   servicesConfig: ServicesConfig
 ) extends FeatureTogglesConfig with AuthConfigProperties {
 
   def getOptional[A](key: String)(implicit loader: ConfigLoader[A]): Option[A] =
     runModeConfiguration.getOptional[A](key)
 
-  def statusRange = s"${TaxYear().prev.year}-${TaxYear().year}"
+  def decorateUrlForLocalDev(key: String): String =
+    runModeConfiguration.getOptional[String](s"external-url.$key").filter(_ => runMode.env == "Dev").getOrElse("")
 
-  lazy val baseURL: String = servicesConfig.baseUrl("tai")
-
-  lazy val citizenAuthHost: String = fetchUrl("citizen-auth")
-  lazy val companyAuthUrl: String = fetchUrl("company-auth")
-  lazy val urlEncode = helper.urlEncode(_: String)
   lazy val incomeTaxFormPartialLinkUrl =
-    s"${fetchUrl("dfs-digital-forms-frontend")}/digital-forms/forms/personal-tax/income-tax/catalogue"
+    s"$dfsFrontendHost/digital-forms/forms/personal-tax/income-tax/catalogue"
   lazy val incomeFromEmploymentPensionPartialLinkUrl =
-    s"${fetchUrl("dfs-digital-forms-frontend")}/digital-forms/form/tell-us-about-income-from-employment-or-pension/draft/guide"
-
+    s"$dfsFrontendHost/digital-forms/form/tell-us-about-income-from-employment-or-pension/draft/guide"
   lazy val incomeFromEmploymentPensionLinkUrl =
-    s"$taiRootUri/digital-forms/form/tell-us-about-income-from-employment-or-pension/draft/guide"
+    s"$dfsFrontendHost/digital-forms/form/tell-us-about-income-from-employment-or-pension/draft/guide"
   lazy val companyBenefitsLinkUrl =
-    s"$taiRootUri/digital-forms/form/tell-us-about-company-benefits/draft/guide"
+    s"$dfsFrontendHost/digital-forms/form/tell-us-about-company-benefits/draft/guide"
   lazy val taxableStateBenefitLinkUrl =
-    s"$taiRootUri/digital-forms/form/tell-us-about-your-taxable-state-benefit/draft/guide"
+    s"$dfsFrontendHost/digital-forms/form/tell-us-about-your-taxable-state-benefit/draft/guide"
   lazy val otherIncomeLinkUrl =
-    s"$taiRootUri/digital-forms/form/tell-us-about-other-income/draft/guide"
+    s"$dfsFrontendHost/digital-forms/form/tell-us-about-other-income/draft/guide"
   lazy val investmentIncomeLinkUrl =
-    s"$taiRootUri/digital-forms/form/tell-us-about-investment-income/draft/guide"
+    s"$dfsFrontendHost/digital-forms/form/tell-us-about-investment-income/draft/guide"
   lazy val taxFreeAllowanceLinkUrl =
-    s"$taiRootUri/digital-forms/form/check-income-tax-tell-us-your-tax-free-allowance/draft/guide"
-
-  lazy val accessibilityStatementToggle: Boolean =
-    getOptional[Boolean]("accessibility-statement.toggle").getOrElse(false)
+    s"$dfsFrontendHost/digital-forms/form/check-income-tax-tell-us-your-tax-free-allowance/draft/guide"
 
   lazy val accessibilityBaseUrl: String = servicesConfig.getString(s"accessibility-statement.baseUrl")
   lazy private val accessibilityRedirectUrl: String = servicesConfig.getString(s"accessibility-statement.redirectUrl")
@@ -65,49 +57,33 @@ class ApplicationConfig @Inject()(
     s"$accessibilityBaseUrl/accessibility-statement$accessibilityRedirectUrl?referrerUrl=${SafeRedirectUrl(
       accessibilityBaseUrl + referrer).encodedUrl}"
 
-  lazy val reportAProblemPartialUrl = s"${fetchUrl("contact-frontend")}/contact/problem_reports?secure=true&service=TAI"
+  lazy val reportAProblemPartialUrl =
+    s"${servicesConfig.baseUrl("contact-frontend")}/contact/problem_reports?secure=true&service=TAI"
   lazy val betaFeedbackUrl = s"$contactHost/contact/beta-feedback"
   lazy val betaFeedbackUnauthenticatedUrl = s"$contactHost/contact/beta-feedback-unauthenticated"
   lazy val urBannerEnabled: Boolean = getOptional[String]("feature.ur-banner.enabled").getOrElse("true").toBoolean
   lazy val urBannerLink: String = getOptional[String]("ur-banner.url").getOrElse("")
-  lazy val checkUpdateProgressLinkUrl = s"${fetchUrl("track")}/track"
-
+  lazy val checkUpdateProgressLinkUrl = s"${servicesConfig.baseUrl("track")}/track"
   lazy val analyticsToken: Option[String] = getOptional[String]("microservice.google-analytics.token")
   lazy val gaValueOfPayments: String =
     getOptional[String]("microservice.google-analytics.gaValueOfPayments").getOrElse("")
   lazy val gaRecStatus: String = getOptional[String]("microservice.google-analytics.gaRecStatus").getOrElse("")
   lazy val analyticsHost: String = getOptional[String]("microservice.google-analytics.host").getOrElse("auto")
-  lazy val pertaxServiceUrl = s"${fetchUrl("pertax-frontend")}/personal-account"
-  lazy val pertaxServiceUpliftFailedUrl: String =
-    getOptional[String]("microservice.external-url.pertax-frontend.host")
-      .getOrElse("") +
-      "/personal-account/identity-check-failed"
-  lazy val pertaxExitSurveyUrl = s"$pertaxServiceUrl/signout?origin=TES"
+  lazy val pertaxServiceUrl = s"$pertaxFrontendHost/personal-account"
+  lazy val pertaxServiceUpliftFailedUrl: String = s"$pertaxFrontendHost/personal-account/identity-check-failed"
   lazy val feedbackSurveyUrl = s"$feedbackHost/feedback/TES"
-  lazy val feedbackHost: String =
-    getOptional[String]("microservice.external-url.feedback-survey-frontend.host").getOrElse("")
-  lazy val cocarFrontendUrl = s"${fetchUrl("cocar-frontend")}/paye/company-car/details"
-  lazy val updateCompanyCarDetailsUrl = s"$personServiceUrl/redirect-company-car"
-  lazy val personServiceUrl = s"${fetchUrl("tai-frontend")}/check-income-tax"
-  lazy val marriageServiceUrl = s"${fetchUrl("tamc-frontend")}/marriage-allowance-application/history"
-  lazy val marriageServiceHistoryUrl = s"${fetchUrl("tamc-frontend")}/marriage-allowance-application/history"
-  lazy val medBenefitServiceUrl = s"${fetchUrl("benefits-frontend")}/paye/benefits/medical-benefit"
-  lazy val mainContentHeaderPartialUrl =
-    s"${fetchUrl("header-service")}/personal-account/integration/main-content-header"
-  lazy val sa16UpliftUrl = s"${fetchUrl("identity-verification-frontend")}/mdtp/uplift"
-  lazy val taiHomePageUrl: String = getOptional[String]("microservice.external-url.tai-frontend.host")
-    .getOrElse("") + "/check-income-tax/what-do-you-want-to-do"
-  lazy val taxYouPaidStatus = s"${fetchUrl("taxcalc-frontend")}/tax-you-paid/status"
+  lazy val cocarFrontendUrl = s"$cocarFrontendHost/paye/company-car/details"
+  lazy val marriageServiceHistoryUrl = s"$tamcFrontendHost/marriage-allowance-application/history"
+  lazy val medBenefitServiceUrl = s"$benefitsFrontendHost/paye/benefits/medical-benefit"
+  lazy val sa16UpliftUrl = s"$identityVerificationHost/mdtp/uplift"
+  lazy val taiHomePageUrl: String = s"$taiRootUri/check-income-tax/what-do-you-want-to-do"
+  lazy val taxYouPaidStatus = s"$taxCalcFrontendHost}/tax-you-paid/status"
   lazy val gg_web_context: String = getOptional[String]("external-url.gg.web-context").getOrElse("gg")
   lazy val ida_web_context: String = getOptional[String]("external-url.ida.web-context").getOrElse("ida")
-  lazy val hardshipHelpBase: String = getOptional[String]("microservice.external-url.hardship-help.host").getOrElse("")
   lazy val hardshipHelpUrl =
-    s"$hardshipHelpBase/digital-forms/form/tell-us-how-you-want-to-pay-estimated-tax/draft/guide"
+    s"$dfsFrontendHost/digital-forms/form/tell-us-how-you-want-to-pay-estimated-tax/draft/guide"
   private val contactHost = getOptional[String](s"microservice.services.contact-frontend.host").getOrElse("")
-  lazy val companyAuthFrontendSignOutUrl = s"$companyAuthUrl/gg/sign-out?continue=$feedbackSurveyUrl"
-  lazy val unauthorisedSignOutUrl: String =
-    getOptional[String]("microservice.external-url.company-auth.unauthorised-url").getOrElse("")
-  lazy val citizenAuthFrontendSignOutUrl: String = citizenAuthHost + "/ida/signout"
+
   lazy val assetsPath =
     s"${getOptional[String](s"assets.url").getOrElse("")}${getOptional[String](s"assets.version").getOrElse("")}/"
   lazy val scottishRateIncomeTaxUrl: String = "https://www.gov.uk/scottish-rate-income-tax"
@@ -121,18 +97,22 @@ class ApplicationConfig @Inject()(
   lazy val frontendTemplatePath: String =
     getOptional[String]("microservice.services.frontend-template-provider.path").getOrElse("/template/mustache")
 
-  lazy val taiRootUri: String = getOptional[String]("microservice.taxPlatformTaiRootUri").getOrElse("")
+  lazy val companyAuthFrontendSignOutUrl = s"$companyAuthUrl/gg/sign-out?continue=$feedbackSurveyUrl"
+  lazy val citizenAuthFrontendSignOutUrl: String = citizenAuthHost + "/ida/signout"
 
-  def fetchUrl(service: String): String =
-    try {
-      servicesConfig.baseUrl(service)
-    } catch {
-      case ex: RuntimeException => taiRootUri
-      case _: Throwable         => s"Unknown Exception: $service-url"
-    }
-
-  lazy val isTaiCy3Enabled: Boolean = getOptional[Boolean]("tai.cy3.enabled").getOrElse(false)
-  lazy val numberOfPreviousYearsToShow: Int = getOptional[Int]("tai.numberOfPreviousYearsToShow").getOrElse(5)
+  //These hosts should be empty for Prod like environments, all frontend services run on the same host so e.g localhost:9030/tai in local should be /tai in prod
+  lazy val citizenAuthHost: String = decorateUrlForLocalDev("citizen-auth.host")
+  lazy val companyAuthUrl: String = decorateUrlForLocalDev("company-auth.host")
+  lazy val feedbackHost: String = decorateUrlForLocalDev("feedback-survey-frontend.host")
+  lazy val unauthorisedSignOutUrl: String = decorateUrlForLocalDev("company-auth.unauthorised-url")
+  lazy val dfsFrontendHost = decorateUrlForLocalDev(s"dfs-digital-forms-frontend.host")
+  lazy val taiRootUri: String = decorateUrlForLocalDev("tai-frontend.host")
+  lazy val pertaxFrontendHost: String = decorateUrlForLocalDev("pertax-frontend.host")
+  lazy val cocarFrontendHost: String = decorateUrlForLocalDev("cocar-frontend.host")
+  lazy val tamcFrontendHost: String = decorateUrlForLocalDev("tamc-frontend.host")
+  lazy val benefitsFrontendHost: String = decorateUrlForLocalDev("benefits-frontend.host")
+  lazy val identityVerificationHost: String = decorateUrlForLocalDev("identity-verification.host")
+  lazy val taxCalcFrontendHost: String = decorateUrlForLocalDev("taxcalc-frontend.host")
 }
 
 trait FeatureTogglesConfig { self: ApplicationConfig =>
@@ -142,18 +122,14 @@ trait FeatureTogglesConfig { self: ApplicationConfig =>
     getOptional[Boolean]("tai.feature.companyCarForceRedirect.enabled").getOrElse(false)
   val cyPlus1EstimatedPayEnabled: Boolean = getOptional[Boolean]("tai.cyPlusOne.enabled").getOrElse(false)
   val webChatEnabled: Boolean = getOptional[Boolean]("tai.webChat.enabled").getOrElse(false)
+  lazy val accessibilityStatementToggle: Boolean =
+    getOptional[Boolean]("accessibility-statement.toggle").getOrElse(false)
+  lazy val isTaiCy3Enabled: Boolean = getOptional[Boolean]("tai.cy3.enabled").getOrElse(false)
+  lazy val numberOfPreviousYearsToShow: Int = getOptional[Int]("tai.numberOfPreviousYearsToShow").getOrElse(5)
 }
 
 trait AuthConfigProperties { self: ApplicationConfig =>
-
   val postSignInRedirectUrl: Option[String] = getOptional[String]("microservice.login-callback.url")
-
-  val activatePaperless: Boolean = getOptional[Boolean]("microservice.activatePaperless")
-    .getOrElse(throw new IllegalStateException("Could not find configuration for microservice.activatePaperless"))
-
-  val activatePaperlessEvenIfGatekeeperFails: Boolean =
-    getOptional[Boolean](s"microservice.activatePaperlessEvenIfGatekeeperFails")
-      .getOrElse(throw new IllegalStateException("Could not find configuration for microservice.activatePaperless"))
 
   val taxPlatformTaiRootUri: String =
     getOptional[String]("microservice.taxPlatformTaiRootUri").getOrElse("http://noConfigTaiRootUri")
