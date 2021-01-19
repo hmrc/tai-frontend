49a7e889e2fcc7bd38247ad2f90680dc11c61d5c
muhammad <muhammad.ahmad@digital.hmrc.gov.uk>
2021-01-19 13:19:43 +0000

    error handler spec updated

--- test/uk/gov/hmrc/tai/config/ErrorHandlerSpec.scala
+++ test/uk/gov/hmrc/tai/config/ErrorHandlerSpec.scala
@@ -16,13 +16,10 @@
 
 import org.jsoup.Jsoup
 import play.api.test.FakeRequest
-import uk.gov.hmrc.tai.config.{ErrorHandler, TaiHtmlPartialRetriever}
-import uk.gov.hmrc.tai.util.viewHelpers.TaiViewSpec
-import views.html.internalServerError
+import uk.gov.hmrc.tai.config.ErrorHandler
 import utils.BaseSpec
 import org.jsoup.nodes.Document
 import play.api.Configuration
-import uk.gov.hmrc.tai.connectors.LocalTemplateRenderer
 
 class ErrorHandlerSpec extends BaseSpec {
   implicit val request = FakeRequest()
@@ -33,10 +30,52 @@ class ErrorHandlerSpec extends BaseSpec {
     app.injector.instanceOf[Configuration]
   )
 
-  "notFoundTemplate" in {
-    def notFound: Document = Jsoup.parse(errorHandler.internalServerErrorTemplate(request).toString())
+  "standardTemplate" in {
+    def doc: Document =
+      Jsoup.parse(
+        errorHandler
+          .standardErrorTemplate(
+            "Sorry, there is a problem with the service",
+            "Sorry, there is a problem with the service",
+            "Try again later.")(request)
+          .toString())
+    doc.getElementsByTag("h1").toString must include(messages("tai.technical.error.heading"))
+    doc.getElementsByTag("p").toString must include(messages("tai.technical.error.message"))
+  }
 
-    notFound.getElementsByTag("h1").toString must include(messages("global.page.not.found.error.heading"))
+  "badRequestTemplate" in {
+    def doc: Document = Jsoup.parse(errorHandler.badRequestTemplate(request).toString())
+    doc.getElementsByTag("h1").toString must include(messages("tai.errorMessage.heading"))
+    doc.getElementsByTag("p").toString must include(messages("tai.errorMessage.frontend400.message1"))
+    doc.getElementsByTag("p").get(1).toString must include(
+      messages(
+        "tai.errorMessage.frontend400.message2",
+        "<a href=\"#report-name\" target=\"_self\" data-sso=\"false\" class=\"report-error__toggle\">" + messages(
+          "tai.errorMessage.reportAProblem") + "</a>"
+      ))
+  }
+
+  "notFoundTemplate" in {
+    def doc: Document = Jsoup.parse(errorHandler.notFoundTemplate(request).toString())
+    doc.getElementsByTag("h1").toString must include(messages("tai.errorMessage.pageNotFound.heading"))
+    doc.getElementsByTag("p").get(0).toString must include(messages("tai.errorMessage.pageNotFound.ifYouTyped"))
+    doc.getElementsByTag("p").get(1).toString must include(messages("tai.errorMessage.pageNotFound.ifYouPasted"))
+    doc.getElementsByTag("p").get(2).toString must include(
+      messages(
+        "tai.errorMessage.pageNotFound.contactHelpline.text",
+        "<a href=\"https://www.gov.uk/government/organisations/hm-revenue-customs/contact/income-tax-enquiries-for-individuals-pensioners-and-employees\" target=\"_self\" data-sso=\"false\">" + messages(
+          "tai.errorMessage.pageNotFound.contactHelpline.link") + "</a>"
+      ))
+  }
+
+  "internalServerErrorTemplate" in {
+    def doc: Document = Jsoup.parse(errorHandler.internalServerErrorTemplate(request).toString())
+
+    doc.getElementsByTag("h1").toString must include(messages("global.error.InternalServerError500.tai.title"))
+    doc.getElementsByTag("p").toString must include(
+      messages("global.error.InternalServerError500.tai.message.you.can") + " <a href=\"https://www.gov.uk/government/organisations/hm-revenue-customs/contact/income-tax-enquiries-for-individuals-pensioners-and-employees\">" + messages(
+        "global.error.InternalServerError500.tai.message.contact.hmrc") + "</a> " + messages(
+        "global.error.InternalServerError500.tai.message.by.phone.post"))
 
   }
 }
