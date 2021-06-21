c9b3171cd4a6064e378c9c2b5bf590ee108a19df
Christian-QA <credfernbus@gmail.com>
2021-06-21 17:01:09 +0100

    Started HttpHandlerSpec with WireMockHelper

--- test/uk/gov/hmrc/tai/connectors/HttpHandlerSpec2.scala
+++ test/uk/gov/hmrc/tai/connectors/HttpHandlerSpec2.scala
@@ -1,5 +1,41 @@
 package uk.gov.hmrc.tai.connectors
 
-class HttpHandlerSpec2 {
+import org.scalatest.{MustMatchers, WordSpec}
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.Messages
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.Injecting
+import uk.gov.hmrc.domain.Generator
+import utils.WireMockHelper
+
+class HttpHandlerSpec2 extends WordSpec with GuiceOneAppPerSuite with MustMatchers with WireMockHelper with ScalaFutures with IntegrationPatience with Injecting {
+
+  val generatedNino = new Generator().nextNino
+
+  val generatedSaUtr = new Generator().nextAtedUtr
+
+  lazy val messages = inject[Messages]
+
+  lazy val httpHandler = inject[HttpHandler]
+
+  "getFromApiV2" must {
+    "should return a json when OK" in {
+
+
+
+
+    }
+
+
+
+
+  }
+
+
+
+
+
+
 
 }
