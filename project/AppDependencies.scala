c5d41b7215f76f2f5b481caa3ed21b17cbc914f7
Vivek <vivek.kumar@digital.hmrc.gov.uk>
2022-12-08 12:48:00 +0000

    PAYEE-984: Upgrade tai-frontend libraries to more recent and stable versions (#1135)
    * PAYEE-984: Upgrade tai-frontend libraries to more recent and stable versions to avoid bobby violations. The versions are selected to avoid incompatibilities among the library dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,25 +3,25 @@ import sbt._
 private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
-  
+
   private val playVersion = "play-28"
 
   val compile = Seq(
     filters,
     jodaForms,
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % s"1.9.0-$playVersion",
+    "org.typelevel"     %% "cats-core"                        % "2.9.0",
+    "com.typesafe.play" %% "play-json-joda"                   % "2.9.3",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % s"1.12.0-$playVersion",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % "5.3.0",
-    "uk.gov.hmrc"       %% "domain"                           % s"6.1.0-$playVersion",
-    "uk.gov.hmrc"       %% "url-builder"                      % s"3.5.0-$playVersion",
-    "uk.gov.hmrc"       %% "play-partials"                    % s"8.1.0-$playVersion",
-    "uk.gov.hmrc"       %% "play-language"                    % s"4.13.0-$playVersion",
-    "uk.gov.hmrc"       %% "local-template-renderer"          % s"2.15.0-$playVersion",
-    "uk.gov.hmrc"       %% "play-ui"                          % s"9.7.0-$playVersion",
-    "org.typelevel"     %% "cats-core"                        % "2.0.0",
-    "com.typesafe.play" %% "play-json-joda"                   % "2.6.10",
-    "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % s"0.18.0-$playVersion",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % s"3.21.0-$playVersion",
-    "uk.gov.hmrc"       %% "play-frontend-pta"             % "0.3.0"
+    "uk.gov.hmrc"       %% "domain"                           % s"8.1.0-$playVersion",
+    "uk.gov.hmrc"       %% "url-builder"                      % s"3.7.0-$playVersion",
+    "uk.gov.hmrc"       %% "play-partials"                    % s"8.3.0-$playVersion",
+    "uk.gov.hmrc"       %% "play-language"                    % s"5.3.0-$playVersion",
+    "uk.gov.hmrc"       %% "local-template-renderer"          % s"2.17.0-$playVersion",
+    "uk.gov.hmrc"       %% "play-ui"                          % s"9.11.0-$playVersion",
+    "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % s"0.25.0-$playVersion",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"               % s"3.34.0-$playVersion",
+    "uk.gov.hmrc"       %% "play-frontend-pta"                % "0.3.0"
   )
 
   trait TestDependencies {
@@ -30,22 +30,20 @@ private object AppDependencies {
   }
 
   object Test {
-    def apply() = new TestDependencies {
-      override lazy val test = Seq(
-        "org.scalatest"            %%    "scalatest"                   % "3.0.9",
-        "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % scope,
-        "org.pegdown"            %  "pegdown"            % "1.6.0" % scope,
-        "org.jsoup"              %  "jsoup"              % "1.8.3" % scope,
-        "org.mockito"            %  "mockito-all"        % "1.10.19" % scope,
-        "com.github.tomakehurst" %  "wiremock-jre8"      % "2.26.1" % scope,
-        "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
-        "org.scalacheck"         %% "scalacheck"         % "1.14.3" % scope
-      )
-    }.test
+    def apply() =
+      new TestDependencies {
+        override lazy val test = Seq(
+          "org.scalatest"          %% "scalatest"          % "3.0.9",
+          "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % scope,
+          "org.pegdown"            % "pegdown"             % "1.6.0" % scope,
+          "org.jsoup"              % "jsoup"               % "1.8.3" % scope,
+          "org.mockito"            % "mockito-all"         % "1.10.19" % scope,
+          "com.github.tomakehurst" % "wiremock-jre8"       % "2.26.1" % scope,
+          "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
+          "org.scalacheck"         %% "scalacheck"         % "1.14.3" % scope
+        )
+      }.test
   }
-
-
 
   def apply() = compile ++ Test()
 }
-
