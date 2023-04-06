eda14a56e9744afa2b489c0b65c0610f02fcd850
masahmad <masahmad7@gmail.com>
2023-04-06 13:04:23 +0100

    minor updates to dependicies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,13 +10,13 @@ private object AppDependencies {
     filters,
     jodaForms,
     "org.typelevel"     %% "cats-core"                        % "2.9.0",
-    "com.typesafe.play" %% "play-json-joda"                   % "2.9.4",
+    "com.typesafe.play" %% "play-json-joda"                   % "2.9.3",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % s"1.12.0-$playVersion",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % "5.3.0",
     "uk.gov.hmrc"       %% "domain"                           % s"8.1.0-$playVersion",
-    "uk.gov.hmrc"       %% "url-builder"                      % s"3.8.0-$playVersion",
+    "uk.gov.hmrc"       %% "url-builder"                      % s"3.7.0-$playVersion",
     "uk.gov.hmrc"       %% "local-template-renderer"          % s"2.17.0-$playVersion",
-    "uk.gov.hmrc"       %% "play-ui"                          % s"9.12.0-$playVersion",
+    "uk.gov.hmrc"       %% "play-ui"                          % s"9.11.0-$playVersion",
     "uk.gov.hmrc"       %% "digital-engagement-platform-chat" % s"0.25.0-$playVersion",
     "uk.gov.hmrc"       %% "play-frontend-hmrc"               % s"3.34.0-$playVersion",
     "uk.gov.hmrc"       %% "play-frontend-pta"                % "0.3.0"
@@ -31,14 +31,14 @@ private object AppDependencies {
     def apply() =
       new TestDependencies {
         override lazy val test = Seq(
-          "org.scalatest"          %% "scalatest"          % "3.0.9"             % scope,
-          "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0"             % scope,
-          "org.pegdown"            % "pegdown"             % "1.6.0"             % scope,
-          "org.jsoup"              % "jsoup"               % "1.15.4"            % scope,
-          "org.mockito"            % "mockito-all"         % "1.10.19"           % scope,
-          "com.github.tomakehurst" % "wiremock-jre8"       % "2.27.2"            % scope,
+          "org.scalatest"          %% "scalatest"          % "3.0.9" % scope,
+          "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % scope,
+          "org.pegdown"            % "pegdown"             % "1.6.0" % scope,
+          "org.jsoup"              % "jsoup"               % "1.8.3" % scope,
+          "org.mockito"            % "mockito-all"         % "1.10.19" % scope,
+          "com.github.tomakehurst" % "wiremock-jre8"       % "2.26.1" % scope,
           "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
-          "org.scalacheck"         %% "scalacheck"         % "1.14.3"            % scope
+          "org.scalacheck"         %% "scalacheck"         % "1.14.3" % scope
         )
       }.test
   }
