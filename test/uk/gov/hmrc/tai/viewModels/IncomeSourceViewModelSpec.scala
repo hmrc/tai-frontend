6f99628cb27bcb9925ce7222e6029102936bf47b
Pascal Parois <pascal22p@parois.net>
2023-08-16 12:19:05 +0100

    fixed merged conflicts

--- test/uk/gov/hmrc/tai/viewModels/IncomeSourceViewModelSpec.scala
+++ test/uk/gov/hmrc/tai/viewModels/IncomeSourceViewModelSpec.scala
@@ -20,6 +20,7 @@ import play.api.i18n.Messages
 import uk.gov.hmrc.tai.model.TaxYear
 import uk.gov.hmrc.tai.model.domain._
 import uk.gov.hmrc.tai.model.domain.income.{Ceased, NonTaxCodeIncome, OtherNonTaxCodeIncome, PotentiallyCeased}
+import uk.gov.hmrc.tai.util.constants.TaiConstants
 import uk.gov.hmrc.tai.util.constants.TaiConstants.{EmployeePensionIForm, InvestIncomeIform, OtherIncomeIform, StateBenefitsIform}
 import utils.{BaseSpec, TaxAccountSummaryTestData}
 
