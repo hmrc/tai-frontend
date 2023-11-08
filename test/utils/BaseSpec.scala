a582badd36a9189d0317243b03f165f261a8e341
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-11-08 15:27:14 +0000

    8019 Add mock auth journey object to base class

--- test/utils/BaseSpec.scala
+++ test/utils/BaseSpec.scala
@@ -16,7 +16,7 @@
 
 package utils
 import builders.UserBuilder
-import controllers.auth.AuthedUser
+import controllers.auth.{AuthJourney, AuthedUser, AuthenticatedRequest, InternalAuthenticatedRequest}
 import controllers.{FakeAuthAction, FakeTaiPlayApplication}
 import org.jsoup.nodes.Element
 import org.mockito.MockitoSugar
@@ -24,7 +24,8 @@ import org.scalatest.BeforeAndAfterEach
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatestplus.play.PlaySpec
 import play.api.i18n._
-import play.api.mvc.MessagesControllerComponents
+import play.api.mvc.{ActionBuilder, AnyContent, BodyParser, MessagesControllerComponents, Request, Result}
+import uk.gov.hmrc.auth.core.ConfidenceLevel
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.mongoFeatureToggles.model.FeatureFlag
@@ -32,12 +33,13 @@ import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.play.language.LanguageUtils
 import uk.gov.hmrc.tai.config.ApplicationConfig
 import uk.gov.hmrc.tai.model.admin.SCAWrapperToggle
+import uk.gov.hmrc.tai.util.constants.TaiConstants
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.reflect.ClassTag
 
 trait BaseSpec
-    extends PlaySpec with FakeTaiPlayApplication with MockitoSugar with I18nSupport with BeforeAndAfterEach
+  extends PlaySpec with FakeTaiPlayApplication with MockitoSugar with I18nSupport with BeforeAndAfterEach
     with ScalaFutures {
 
   def inject[T](implicit evidence: ClassTag[T]): T = app.injector.instanceOf[T]
@@ -51,6 +53,8 @@ trait BaseSpec
   implicit lazy val provider: MessagesProvider = inject[MessagesProvider]
   implicit lazy val lang: Lang = Lang("en")
   implicit lazy val messages: Messages = messagesApi.preferred(Seq(lang))
+
+  protected lazy val mockAuthJourney: AuthJourney = mock[AuthJourney]
 
   val nino: Nino = FakeAuthAction.nino
 
@@ -69,8 +73,53 @@ trait BaseSpec
 
   override def beforeEach(): Unit = {
     reset(mockFeatureFlagService)
+    reset(mockAuthJourney)
     when(mockFeatureFlagService.get(SCAWrapperToggle))
       .thenReturn(Future.successful(FeatureFlag(SCAWrapperToggle, isEnabled = false)))
+
+    when(mockAuthJourney.authWithValidatePerson).thenReturn(new ActionBuilder[AuthenticatedRequest, AnyContent] {
+      private val user =
+        AuthedUser(
+          Nino(nino.toString()),
+          Some("saUtr"),
+          Some(TaiConstants.AuthProviderGG),
+          ConfidenceLevel.L200,
+          None,
+          None
+        )
+      override def invokeBlock[A](
+                                   request: Request[A],
+                                   block: AuthenticatedRequest[A] => Future[Result]
+                                 ): Future[Result] =
+        block(AuthenticatedRequest(request, user, "testUser"))
+
+      override def parser: BodyParser[AnyContent] = mcc.parsers.defaultBodyParser
+
+      override protected def executionContext: ExecutionContext = ec
+    })
+
+    when(mockAuthJourney.authWithoutValidatePerson).thenReturn(new ActionBuilder[InternalAuthenticatedRequest, AnyContent] {
+      private val user =
+        AuthedUser(
+          Nino(nino.toString()),
+          Some("saUtr"),
+          Some(TaiConstants.AuthProviderGG),
+          ConfidenceLevel.L200,
+          None,
+          None
+        )
+
+      override def invokeBlock[A](
+                                   request: Request[A],
+                                   block: InternalAuthenticatedRequest[A] => Future[Result]
+                                 ): Future[Result] =
+        block(InternalAuthenticatedRequest(request, user))
+
+      override def parser: BodyParser[AnyContent] = mcc.parsers.defaultBodyParser
+
+      override protected def executionContext: ExecutionContext = ec
+    })
+
   }
 
 }
