bdf0478936285e841fa1c74c573d5806bfb22232
grantrobson <grant.robson@digital.hmrc.gov.uk>
2024-05-20 12:52:21 +0100

    8408 Remove jackson overrides

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,8 +13,8 @@ private object AppDependencies {
     "org.typelevel" %% "cats-core"                                   % "2.10.0",
     "uk.gov.hmrc"   %% s"play-conditional-form-mapping-$playVersion" % "2.0.0",
     "uk.gov.hmrc"   %% "digital-engagement-platform-chat-29"         % webChatVersion,
-    "uk.gov.hmrc" %% s"mongo-feature-toggles-client-$playVersion" % "1.3.0",
-    "uk.gov.hmrc" %% s"sca-wrapper-$playVersion"                  % "1.6.0"
+    "uk.gov.hmrc"   %% s"mongo-feature-toggles-client-$playVersion"  % "1.3.0",
+    "uk.gov.hmrc"   %% s"sca-wrapper-$playVersion"                   % "1.6.0"
   )
 
   val test: Seq[ModuleID] = Seq(
@@ -26,26 +26,6 @@ private object AppDependencies {
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion
   ).map(_ % "test")
 
-  val jacksonVersion = "2.13.2"
-  val jacksonDatabindVersion = "2.13.2.2"
-
-  val jacksonOverrides: Seq[ModuleID] = Seq(
-    "com.fasterxml.jackson.core"     % "jackson-core",
-    "com.fasterxml.jackson.core"     % "jackson-annotations",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8",
-    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310"
-  ).map(_ % jacksonVersion)
-
-  val jacksonDatabindOverrides: Seq[ModuleID] = Seq(
-    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonDatabindVersion
-  )
-
-  val akkaSerializationJacksonOverrides: Seq[ModuleID] = Seq(
-    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-cbor",
-    "com.fasterxml.jackson.module"     % "jackson-module-parameter-names",
-    "com.fasterxml.jackson.module"    %% "jackson-module-scala"
-  ).map(_ % jacksonVersion)
-
   val all: Seq[ModuleID] =
-    compile ++ jacksonDatabindOverrides ++ jacksonOverrides ++ akkaSerializationJacksonOverrides ++ test
+    compile ++ test
 }
