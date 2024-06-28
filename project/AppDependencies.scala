0e37236d333240f9bd61732a865d53836c5d2aff
askar.makkunnath <askar.makkunnath@digital.hmrc.gov.uk>
2024-06-28 16:33:43 +0100

    DDCNL-9072: Upgraded Libraries and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import play.sbt.PlayImport.*
 import sbt.*
 
 private object AppDependencies {
-  private val playVersion = "play-29"
+  private val playVersion = "play-30"
   private val hmrcMongoVersion = "1.9.0"
   private val bootstrapVersion = "8.6.0"
-  private val webChatVersion = "1.4.0"
+  private val webChatVersion = "1.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     filters,
-    "org.typelevel" %% "cats-core"                                   % "2.10.0",
+    "org.typelevel" %% "cats-core"                                   % "2.12.0",
     "uk.gov.hmrc"   %% s"play-conditional-form-mapping-$playVersion" % "2.0.0",
     "uk.gov.hmrc"   %% "digital-engagement-platform-chat-29"         % webChatVersion,
-    "uk.gov.hmrc"   %% s"mongo-feature-toggles-client-$playVersion"  % "1.4.0",
-    "uk.gov.hmrc"   %% s"sca-wrapper-$playVersion"                   % "1.8.0"
+    "uk.gov.hmrc"   %% s"mongo-feature-toggles-client-$playVersion"  % "1.5.0",
+    "uk.gov.hmrc"   %% s"sca-wrapper-$playVersion"                   % "1.9.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
-    "org.mockito"       %% "mockito-scala-scalatest"       % "1.17.31",
+    "org.mockito"       %% "mockito-scala-scalatest"       % "1.17.37",
     "org.scalatestplus" %% "scalacheck-1-17"               % "3.2.18.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
     "org.jsoup"          % "jsoup"                         % "1.17.2"
