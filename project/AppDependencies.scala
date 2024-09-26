e8fbeb155fce662def6f2eaf537873339beac189
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-09-26 09:08:46 +0100

    update sca-wrapper with fix to backlink from survey

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ private object AppDependencies {
   private val hmrcMongoVersion = "1.9.0"
   private val bootstrapVersion = "8.6.0"
   private val webChatVersion = "1.6.0"
-  private val scaWrapperVersion = "1.12.0"
+  private val scaWrapperVersion = "1.13.0"
   private val mongoFeatureToggleVersion = "1.6.0"
 
   val compile: Seq[ModuleID] = Seq(
